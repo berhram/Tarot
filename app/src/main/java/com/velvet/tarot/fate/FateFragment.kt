b65037a0bca304cb112d69f727b205d094627d15
Daniil <berhram@protonmail.com>
2022-03-15 01:19:58 +0300

    some unimportant fixes

--- app/src/main/java/com/velvet/tarot/fate/FateFragment.kt
+++ app/src/main/java/com/velvet/tarot/fate/FateFragment.kt
@@ -1,7 +1,9 @@
 package com.velvet.tarot.fate
 
+import android.content.res.Resources
 import android.graphics.Paint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,12 +19,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.fragment.app.viewModels
 import com.velvet.mvi.HostedFragment
 import com.velvet.tarot.R
@@ -56,26 +59,33 @@ fun FateScreen(
 ) {
     MaterialTheme() {
         Scaffold() {
-            Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Bottom) {
-                Row(modifier = Modifier.align(Alignment.CenterHorizontally), horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
-                    OneCard(id = "wapa")
-                    OneCard(id = "wakn")
-                    OneCard(id = "waki")
+            ConstraintLayout(Modifier.fillMaxSize()) {
+                val (buttons, cards) = createRefs()
+                Box(modifier = Modifier.constrainAs(cards) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(buttons.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }) {
+                    ThreeCard(first = "waki", second = "cuki", third = "ar16")
                 }
                 Row(
                     Modifier
                         .padding(10.dp)
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
-                    Button(onClick = { /*TODO*/ }) {
-                        Text(text = "get one card")
+                        .constrainAs(buttons) {
+                            bottom.linkTo(parent.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        },
+                    horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.Bottom) {
+                    Button(onClick = { model.getCards(1) }) {
+                        Text(text = "One card")
                     }
                     Spacer(modifier = Modifier.size(10.dp))
                     Button(onClick = { /*TODO*/ }) {
-                        Text(text = "get two card")
+                        Text(text = "Two cards")
                     }
                 }
-
             }
         }
     }
@@ -83,10 +93,28 @@ fun FateScreen(
 
 @Composable
 fun OneCard(id: String) {
-    Card(id = id)
+    Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
+        Box(modifier = Modifier.weight(1f)) {
+            Card(id = id)
+        }
+    }
 }
 
-@Preview
+@Composable
+fun ThreeCard(first: String, second: String, third: String) {
+    Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
+        Box(modifier = Modifier.weight(1f)) {
+            Card(id = first)
+        }
+        Box(modifier = Modifier.weight(1f)) {
+            Card(id = second)
+        }
+        Box(modifier = Modifier.weight(1f)) {
+            Card(id = third)
+        }
+    }
+}
+
 @Composable
 fun Cardholder() {
     Image(painter = painterResource(id = R.drawable.tarot_back_pale), contentDescription = "Cardholder")
@@ -96,11 +124,22 @@ fun Cardholder() {
 fun Card(id: String) {
     val context = LocalContext.current
     val drawableId = remember(id) {
-        context.resources.getIdentifier(
+        var out = context.resources.getIdentifier(
             id,
             "drawable",
             context.packageName
         )
+        if (out == 0) {
+            out = context.resources.getIdentifier(
+                "no_such_card",
+                "drawable",
+                context.packageName
+            )
+        }
+        out
     }
-    Image(painter = painterResource(id = drawableId), contentDescription = "Card", contentScale = ContentScale.Fit)
+    Box(
+        Modifier.padding(10.dp)) {
+        Image(painter = painterResource(id = drawableId), contentDescription = "Card", contentScale = ContentScale.Fit)
+    }
 }
--- app/src/main/java/com/velvet/tarot/fate/FateViewModel.kt
+++ app/src/main/java/com/velvet/tarot/fate/FateViewModel.kt
@@ -1,8 +1,21 @@
 package com.velvet.tarot.fate
 
+import com.velvet.models.network.Network
 import com.velvet.mvi.MviViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
 
-class FateViewModel : MviViewModel<FateContract.View, FateScreenState, FateScreenEffect>(),
+class FateViewModel(private val repo: Network) : MviViewModel<FateContract.View, FateScreenState, FateScreenEffect>(),
     FateContract.ViewModel {
+
+    private val tarotIdsFlow = MutableStateFlow(-1L)
+
+    private var launch: Job? = null
+
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, e ->
+        setAction(FateScreenEffect.Error(e))
+    }
 
 }
