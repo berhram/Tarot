d1f9778dd3667b10d878efddfef78edb3fff74df
Daniil <berhram@protonmail.com>
2022-03-21 00:01:43 +0300

    backup commit

--- models/src/main/java/com/velvet/models/network/NetworkImpl.kt
+++ models/src/main/java/com/velvet/models/network/NetworkImpl.kt
@@ -1,16 +1,19 @@
 package com.velvet.models.network
 
+import android.util.Log
 import com.velvet.models.BuildConfig
 import com.velvet.models.card.CardDetailsScheme
+import com.velvet.models.card.CardListScheme
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
+import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
-import kotlin.collections.ArrayList
+import javax.inject.Inject
 
-class NetworkImpl : Network {
-    private val service: RetrofitApi
+
+class NetworkImpl @Inject constructor(interceptor: ConnectionInterceptor) : Network {
+    private var service: RetrofitApi
 
     init {
         val logging = HttpLoggingInterceptor()
@@ -21,9 +24,12 @@ class NetworkImpl : Network {
         }
         val httpClient = OkHttpClient.Builder()
         httpClient.addInterceptor(logging)
+        //httpClient.addInterceptor(interceptor)
+        Log.d("HTTP", "add interceptor")
         httpClient.readTimeout(10, TimeUnit.SECONDS)
         httpClient.writeTimeout(10, TimeUnit.SECONDS)
         httpClient.connectTimeout(10, TimeUnit.SECONDS)
+        Log.d("HTTP", "builder")
         val retrofit = Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .client(httpClient.build())
@@ -32,12 +38,23 @@ class NetworkImpl : Network {
         service = retrofit.create(RetrofitApi::class.java)
     }
 
-    override suspend fun getCards() : List<CardDetailsScheme> {
-        val cards = service.getCards().cards?.let {
+    private fun CardListScheme.toListSchemas() : List<CardDetailsScheme> {
+        val cards = this.cards?.let {
             it
         } ?: kotlin.run {
             ArrayList()
         }
         return cards
     }
+
+    override suspend fun getCards() : Result<List<CardDetailsScheme>> {
+        Log.d("HTTP", "getCards invoked")
+        val cards = service.getCards().awaitResponse().body()
+        return if (cards != null) {
+            Result.success(cards.toListSchemas())
+        } else {
+            Result.failure(Exception("Can't retrieve cards"))
+        }
+    }
 }
+
