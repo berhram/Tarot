cc5dfac327d34047fa3348f75d0a0df09807b792
Daniil <berhram@protonmail.com>
2022-03-23 03:10:19 +0300

    ui update: search, filters added

--- app/src/main/java/com/velvet/tarot/card/CardScreen.kt
+++ app/src/main/java/com/velvet/tarot/card/CardScreen.kt
@@ -4,34 +4,45 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.velvet.data.card.Card
 import com.velvet.tarot.R
 import com.velvet.tarot.theme.AppTheme
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun CardScreen(viewModel: CardViewModel, onBack: () -> Unit) {
     val state = viewModel.container.stateFlow.collectAsState()
+    LaunchedEffect(viewModel) {
+        viewModel.container.sideEffectFlow.collectLatest {
+            when (it) {
+                is CardEffect.GoBack -> onBack()
+            }
+        }
+    }
     Scaffold(topBar = {
-        TopAppBar(
-            title = { Text(text = state.value.card.name,
-                style = AppTheme.typography.h1,
-                textAlign = TextAlign.Start, color = AppTheme.colors.textPrimary)},
-            navigationIcon = {
-                IconButton(onClick = onBack) {
+        TopAppBar(backgroundColor = AppTheme.colors.background, elevation = 0.dp, modifier = Modifier.fillMaxWidth()) {
+            Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween) {
+                Text(text = state.value.card.name.lowercase().replace(" ", "_") + stringResource(
+                    id = R.string.card_dir),
+                    style = AppTheme.typography.h1,
+                    textAlign = TextAlign.Start,
+                    color = AppTheme.colors.textPrimary)
+                IconButton(onClick = { viewModel.goBack() }) {
                     Icon(
-                        Icons.Filled.ArrowBack,
-                        contentDescription = stringResource(id = R.string.button_back), tint = AppTheme.colors.textPrimary
+                        painter = painterResource(id = R.drawable.ic_q), contentDescription = stringResource(
+                            id = R.string.button_back
+                        ), tint = AppTheme.colors.textPrimary
                     )
                 }
-            }, backgroundColor = AppTheme.colors.background
-        )
+            }
+        }
     }, backgroundColor = AppTheme.colors.background) {
         ShowCard(card = state.value.card)
     }
--- app/src/main/java/com/velvet/tarot/feed/FeedScreen.kt
+++ app/src/main/java/com/velvet/tarot/feed/FeedScreen.kt
@@ -1,22 +1,26 @@
 package com.velvet.tarot.feed
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.velvet.data.Strings
 import com.velvet.data.card.Card
 import com.velvet.tarot.R
 import com.velvet.tarot.theme.AppTheme
@@ -24,46 +28,79 @@ import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun FeedScreen(viewModel: FeedViewModel, onShowCard: (cardName: String) -> Unit) {
+    val state = viewModel.container.stateFlow.collectAsState()
+    LaunchedEffect(viewModel) {
+        viewModel.container.sideEffectFlow.collectLatest {
+            when (it) {
+                is FeedEffect.ShowCard -> onShowCard(it.cardName)
+            }
+        }
+    }
     Scaffold(topBar = {
-        TopAppBar(
-            title = { Text(text = stringResource(id = R.string.app_name), style = AppTheme.typography.h1,
-                textAlign = TextAlign.Start, color = AppTheme.colors.textPrimary) }, backgroundColor = AppTheme.colors.background
-        )
-    }, backgroundColor = AppTheme.colors.background) {
-        val state = viewModel.container.stateFlow.collectAsState()
-        LaunchedEffect(viewModel) {
-            viewModel.container.sideEffectFlow.collectLatest {
-                when (it) {
-                    is FeedEffect.ShowCard -> onShowCard(it.cardName)
+        TopAppBar(backgroundColor = AppTheme.colors.background, elevation = 0.dp, modifier = Modifier.fillMaxWidth()) {
+            Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween) {
+                Text(text = stringResource(id = R.string.tarot_dir),
+                    style = AppTheme.typography.h1,
+                    textAlign = TextAlign.Start,
+                    color = AppTheme.colors.textPrimary)
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    DropdownMenu(expanded = state.value.isExpanded, onDismissRequest = { viewModel.filterClick() }, modifier = Modifier.background(AppTheme.colors.background)) {
+                        DropdownMenuItem(onClick = { viewModel.setFilter(Strings.Major) }) {
+                            val check = if (state.value.filter.isMajorEnabled) "[X] - " else "[ ] - "
+                            Text(text = check + Strings.Major,
+                                style = AppTheme.typography.body1,
+                                textAlign = TextAlign.Start,
+                                color = AppTheme.colors.textPrimary)
+                        }
+                        DropdownMenuItem(onClick = { viewModel.setFilter(Strings.Minor) } , ) {
+                            val check = if (state.value.filter.isMinorEnabled) "[X] - " else "[ ] - "
+                            Text(text = check + Strings.Minor,
+                                style = AppTheme.typography.body1,
+                                textAlign = TextAlign.Start,
+                                color = AppTheme.colors.textPrimary)
+                        }
+                    }
+                    IconButton(onClick = { viewModel.filterClick() },
+                        modifier = if (state.value.filter.isEnable()) Modifier.background(AppTheme.colors.effect) else Modifier.background(AppTheme.colors.background)) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_filter), contentDescription = stringResource(
+                                id = R.string.button_filter
+                            ), tint = AppTheme.colors.textPrimary
+                        )
+                    }
                 }
             }
         }
-        SwipeRefresh(
-            state = rememberSwipeRefreshState(isRefreshing = state.value.isLoading),
-            onRefresh = { viewModel.refresh() },
-            modifier = Modifier.fillMaxSize()
-        ) {
-            LazyColumn {
-                if (state.value.cards.isNullOrEmpty()) {
-                    items(items = listOf(System.currentTimeMillis()), key = { it }) {
-                        Column(
-                            modifier = Modifier
-                                .fillParentMaxHeight()
-                                .fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center,
-                        ) {
-                            Text(
-                                textAlign = TextAlign.Center,
-                                text = stringResource(id = R.string.no_cards),
-                            )
+    }, backgroundColor = AppTheme.colors.background) {
+        Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+            SearchBar(searchText = state.value.searchText, onChangedSearchText = { viewModel.searchCard(it) })
+            SwipeRefresh(
+                state = rememberSwipeRefreshState(isRefreshing = state.value.isLoading),
+                onRefresh = { viewModel.refresh() },
+                modifier = Modifier.fillMaxSize()
+            ) {
+                LazyColumn {
+                    if (state.value.cards.isNullOrEmpty()) {
+                        items(items = listOf(System.currentTimeMillis()), key = { it }) {
+                            Column(
+                                modifier = Modifier
+                                    .fillParentMaxHeight()
+                                    .fillMaxWidth(),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center,
+                            ) {
+                                Text(
+                                    textAlign = TextAlign.Center,
+                                    text = stringResource(id = R.string.no_cards),
+                                )
+                            }
                         }
+                    } else {
+                        items(
+                            items = state.value.cards,
+                            key = { it.name }
+                        ) { CardItem(it, viewModel = viewModel) }
                     }
-                } else {
-                    items(
-                        items = state.value.cards,
-                        key = { it.name }
-                    ) { CardItem(it, viewModel = viewModel) }
                 }
             }
         }
@@ -82,6 +119,29 @@ fun CardItem(card: Card, viewModel: FeedViewModel) {
             style = AppTheme.typography.h1,
             textAlign = TextAlign.Start,
             color = AppTheme.colors.textPrimary
+        )
+    }
+}
+
+@Composable
+fun SearchBar(
+    searchText: String,
+    onChangedSearchText: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        OutlinedTextField(
+            value = searchText,
+            textStyle = AppTheme.typography.body1,
+            onValueChange = onChangedSearchText,
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = AppTheme.colors.effect,
+                unfocusedBorderColor = AppTheme.colors.primary,
+                textColor = AppTheme.colors.textPrimary,
+                cursorColor = AppTheme.colors.effect
+            ),
+            label = { Text(text =  stringResource(R.string.search), color = AppTheme.colors.textPrimary, style = AppTheme.typography.subtitle) }
         )
     }
 }
