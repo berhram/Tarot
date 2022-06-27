2933c206d2d752f2c0de1a15a9c0c200bf34b316
velvet <berhram@protonmail.com>
2022-06-27 22:37:01 +0300

    feat: base mvi classes (WIP)

---
+++ coremvi/src/main/kotlin/com/velvet/coremvi/SafeCall.kt
@@ -0,0 +1,13 @@
+package com.velvet.coremvi
+
+interface SafeCall {
+    suspend fun <T> safeCall(
+        call: suspend () -> T
+    ) : Result<T> {
+        return try {
+            Result.success(call.invoke())
+        } catch (e: Throwable) {
+            Result.failure(e)
+        }
+    }
+}
