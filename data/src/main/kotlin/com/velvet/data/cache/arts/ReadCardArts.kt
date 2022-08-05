fa46c8533a90371da0bab69a66fb448e3d8ac4dc
velvet <berhram@protonmail.com>
2022-08-05 13:13:08 +0300

    tests for cloud tarot datastore

--- data/src/main/kotlin/com/velvet/data/cache/arts/ReadCardArts.kt
+++ data/src/main/kotlin/com/velvet/data/cache/arts/ReadCardArts.kt
@@ -7,10 +7,9 @@ import com.velvet.core.Read
 import com.velvet.data.schemas.CardArt
 import java.lang.reflect.Type
 
-//TODO filename
 class ReadCardArts(private val gson: Gson, private val manageResources: ManageResources) : Read<List<CardArt>> {
 
     private val typeList: Type = object : TypeToken<List<CardArt>>() {}.type
 
-    override fun read(): List<CardArt> = gson.fromJson(manageResources.string(""), typeList)
+    override fun read(): List<CardArt> = gson.fromJson(manageResources.string("ASCIIarts.json"), typeList)
 }
---
+++ data/src/test/kotlin/com/velvet/data/cloud/TarotCloudDataSourceTest.kt
@@ -0,0 +1,54 @@
+package com.velvet.data.cloud
+
+import com.velvet.core.exception.HandleError
+import com.velvet.data.schemas.Card
+import com.velvet.data.schemas.RemoteCards
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+
+internal class TarotCloudDataSourceTest {
+
+    private val foundCards = listOf(
+        Card("0", "The Death", "Major", "Nice", "Not nice", "A saddest one card"),
+        Card("3", "The Life", "Major", "Hehe", "Not hehe", "A hehest one card")
+    )
+
+    private val allCards = listOf(
+        Card("0", "The Death", "Major", "Nice", "Not nice", "A saddest one card"),
+        Card("1", "The Moon", "Major", "Hot", "Cold", "A hottest one card"),
+        Card("2", "The Sun", "Major", "Cold", "Hot", "A coldest one card"),
+        Card("3", "The Life", "Major", "Hehe", "Not hehe", "A hehest one card")
+    )
+
+    @Test
+    fun `success cards by keyword`() = runBlocking {
+        val tarotCloudDataSource = TarotCloudDataSource.Base(TestService(foundCards, allCards), TestHandleError())
+        Assert.assertEquals(foundCards, tarotCloudDataSource.cards("someKeyword"))
+    }
+
+    @Test
+    fun `failed cards by keyword`() = runBlocking {
+        val tarotCloudDataSource = TarotCloudDataSource.Base(TestService(foundCards, allCards), TestHandleError())
+        Assert.assertEquals(emptyList<Card>(), tarotCloudDataSource.cards("abrakadabra"))
+    }
+
+    @Test
+    fun `success all cards`() = runBlocking {
+        val tarotCloudDataSource = TarotCloudDataSource.Base(TestService(foundCards, allCards), TestHandleError())
+        Assert.assertEquals(allCards, tarotCloudDataSource.cards())
+    }
+
+    private class TestHandleError : HandleError {
+
+        override fun handle(error: Exception): Exception = error
+    }
+
+    private class TestService(private val foundCards: List<Card>, private val allCards: List<Card>) : TarotService {
+
+        override fun searchByKeyword(keyword: String): RemoteCards =
+            RemoteCards(if (keyword == "someKeyword") foundCards else emptyList())
+
+        override fun allCards(): RemoteCards = RemoteCards(allCards)
+    }
+}
